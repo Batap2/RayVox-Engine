542ecbc2d85bffc2fc93dcbc8786450ca95cbb59
Batap <batapxv7@gmail.com>
2024-08-07 17:33:15 +0200

    up

--- out/build/x64-Debug/include/DirectX-Headers/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Dossier_Bat/ProjetProg/RayVox-Engine/include/DirectX-Headers
-# Build directory: D:/Dossier_Bat/ProjetProg/RayVox-Engine/out/build/x64-Debug/include/DirectX-Headers
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
